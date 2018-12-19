c47a91c7628f105afd5d754ef0309ff9889c1c3e
sho.koma <aaa@gmail.com>
2018-12-19 08:21:02 +0000

    kurou sita kedo dekita

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -13,18 +13,41 @@ class TasksController < ApplicationController
   end
   
   def create
+    @task = Task.new(task_params)
+    if @task.save
+      flash[:success] = '新しいタスクが作られました'
+      redirect_to tasks_path
+    else
+      flash.now[:danger] = 'ERRORが起きました。やり直して下さい'
+      render :new
+    end
   end
   
   def edit
+    @task = Task.find(params[:id])
   end
   
   def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      flash[:success] = 'タスクが更新されました'
+      redirect_to tasks_path
+    else
+      flash.now[:danger] = 'ERRORが起きました。やり直して下さい'
+      render :edit
+    end
   end
   
   def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    
+    flash[:success] = 'タスクが削除されました'
+    redirect_to @task
   end
   
   private
-  def xxx
+  def task_params
+    params.require(:task).permit(:content)
   end
 end
